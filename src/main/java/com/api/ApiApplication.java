d7b73f19bd4f84ab4d70912525bc557acac05f96
Belen <anabelensanchezboy@gmail.com>
2025-09-03 01:37:45 -0500

    Cambio contraseña y uso de variables de entorno: .env

--- src/main/java/com/api/ApiApplication.java
+++ src/main/java/com/api/ApiApplication.java
@@ -2,12 +2,16 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
